fb588b35a8f8870bf517b11a1e3f17792823f5c6
alqof <alqofnuraji@gmail.com>
2025-02-18 08:46:06 +0000

    Intial Commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+import { inter, lusitana } from '@/app/ui/fonts';
+
+export default function Page(){
+    return (
+        <div className="px-3 py-4 md:px-2">
+            <div className="rounded-md bg-gray-50">
+                <h1 className={`mb-4 font-bold ${lusitana.className} text-xl md:text-3xl`}> Invoices </h1>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dicta minima neque? Asperiores iusto excepturi omnis est voluptas perspiciatis at magni tempora quia, rem recusandae molestiae, consectetur exercitationem commodi enim quaerat quasi nesciunt? Illo vel dolor eius, distinctio tempore ab dolores maiores amet odit fugit aliquid aut, minus officiis adipisci excepturi culpa deserunt laudantium delectus ut blanditiis temporibus. Praesentium sint, fugit eum consectetur, velit rem earum explicabo ipsa, porro est eveniet laudantium. Beatae commodi minima porro perspiciatis ea dolorum, omnis voluptatum blanditiis, exercitationem adipisci iste. Ea autem fugiat distinctio fuga id excepturi iure sed, dolorem omnis velit totam. Enim, corporis!</p>
+            </div>
+        </div>
+    );
+}
