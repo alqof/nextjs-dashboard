f6cc0c0dc7ac43a6988ef3754bcdbeac099dad66
alqof <alqofnuraji@gmail.com>
2025-02-18 11:10:25 +0000

    add Recent Revenue - dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,6 @@ import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page(){
     const revenue = await fetchRevenue();
-    console.log(revenue)
 
     return (
         <main>
